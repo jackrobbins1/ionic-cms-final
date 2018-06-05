9434e3e91ae7dc0f81516c8e84f74ba79d455c50
Jackson Robbins <jackrobbins1@gmail.com>
2018-06-05 10:47:50 -0500

    added recent updates

---
+++ src/pages/article-create/article-create.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+import { Article } from '../../models/article/article';
+import { ArticleProvider } from '../../providers/article/article';
+
+import { ArticlePage } from '../article/article';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-article-create',
+  templateUrl: 'article-create.html',
+  styles: ['user-create']
+})
+export class ArticleCreatePage {
+
+  public article: FormGroup;
+  public errors: Array<any> = [];
+  public errorMessage: string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private articleProvider: ArticleProvider,
+    private formBuilder: FormBuilder
+  ) {
+    this.article = this.formBuilder.group({
+      title: [],
+    });
+  }
+
+  response(response: any): void{
+    if(response.success === false){
+      this.errors = response.error.errors;
+      this.errorMessage = response.error._message;
+    }
+
+    if(response.success === true){
+      this.navCtrl.push(ArticlePage, { slug: response.article.slug });
+    }
+  }
+
+  public createArticle(): void{
+    this.articleProvider.createArticle(this.article.value).subscribe(
+      (response:any)=>{
+        this.response(response);
+      }
+    );
+  }
+
+}
---
+++ src/pages/article/article.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { ArticleProvider } from '../../providers/article/article';
+import { Article } from '../../models/article/article';
+
+import { ArticleEditPage } from '../article-edit/article-edit';
+import { ArticleDeletePage } from '../article-delete/article-delete';
+
+@IonicPage()
+@Component({
+  selector: 'page-article',
+  templateUrl: 'article.html',
+})
+export class ArticlePage {
+
+  public article: Article;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private articleProvider: ArticleProvider
+  ) {
+    this.getArticle(this.navParams.data.slug);
+  }
+
+  private getArticle(slug: string): void{
+    this.articleProvider.getArticle(slug).subscribe(
+      (response:any)=>{
+        console.log(response);
+        this.article = response.article;
+      }
+    );
+  }
+
+  public toArticleEdit(){
+    this.navCtrl.push(ArticleEditPage, { slug: this.article.slug });
+  }
+
+  public toArticleDelete(){
+    this.navCtrl.push(ArticleDeletePage, {slug: this.article.slug });
+  }
+
+}
